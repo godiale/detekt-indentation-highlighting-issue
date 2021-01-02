c1e3b7a8c71fd2b4d9e4dcf463315a4b2507545f
godiale <11892279+godiale@users.noreply.github.com>
2021-01-02 21:02:03 +0300

    Configure detekt with formatting rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import io.gitlab.arturbosch.detekt.Detekt
 
 plugins {
     kotlin("jvm") version "1.4.21"
+    id("io.gitlab.arturbosch.detekt") version "1.15.0"
 }
 
 group = "my.demo"
@@ -9,8 +11,21 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "11"
+}
+
+tasks.withType<Detekt>().configureEach {
+    this.jvmTarget = "11"
+}
+
+detekt {
+    config = files("config/detekt.yml")
+}
+
+dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.15.0")
 }
